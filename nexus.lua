a7956902c782d84d876382a75b89ff108bbb1b33
qwerudu1337 <161774196+qwerudu1337@users.noreply.github.com>
2025-06-28 03:34:47 +0300

    Update nexus.lua

--- nexus.lua
+++ nexus.lua
@@ -232,7 +232,7 @@ do -- Nexus
 
             local Success, Socket = pcall(WSConnect, ('ws://%s/Nexus?name=%s&id=%s&jobId=%s'):format(Host, LocalPlayer.Name, LocalPlayer.UserId, game.JobId))
 
-            local placeId = LocalPlayer.placeId
+            local placeId = LocalPlayer.PlaceId
 
             if not Success then task.wait(12) continue end
 
